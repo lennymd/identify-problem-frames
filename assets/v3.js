083567bc3efca2cb83e6fbfa08ac514b9c163236
Lenny Martinez <lennymartinezd@gmail.com>
2023-02-02 11:34:30 +0100

    start work with v3

---
+++ assets/v3.js
@@ -0,0 +1,8 @@
+// In v3 we read and process multiple text files. We use the functionality from v2 and v1 to load and process files, and in v3 we'll add the ability to see the conversation and the ngrams.
+
+async function main() {
+  console.log('v3 Hello World!');
+
+  
+}
+main();
