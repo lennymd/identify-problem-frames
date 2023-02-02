d3d9d8fcd18f4d2c7d8a7c0753a2b3391cac32f5
Lenny Martinez <lennymartinezd@gmail.com>
2023-02-02 16:14:14 +0100

    Update v3.js
    Load contents from multiple files to work with.

--- assets/v3.js
+++ assets/v3.js
@@ -3,19 +3,22 @@
 async function main() {
   console.log('v3 Hello World!');
   let container = d3.select('#container');
-  let _status = d3.select('#status');
+  //  let _status = d3.select('#status');
   let submit_button = d3.select('#submit_button');
   let file_selector = d3.select('#file_selector');
-  let file_list, files_to_read, files_data;
+  let file_list, files_to_read;
+  let conversation_data_all;
+
+  // On Submit, get all the files that we're going to read.
   submit_button.on('click', () => {
     // First, check if we have any files to process.
     file_list = Array.from(file_selector.node().files);
     files_to_read = [];
-    files_data = [];
+    conversation_data_all = [];
     if (file_list.length > 0) {
-      // There is at least 1 file to process. Check if the format is something usable. Usable here means:
-      // 1. File supports "file.type" property.
-      // 2. File.type property is "text/*".
+      // There is at least 1 file to process. Check if the format is something usable.
+      // Usable here means:
+      // (File supports "file.type" property) AND (file.type is "text/*)
       // Copy usable files to files_to_read.
 
       file_list.forEach(file => {
@@ -25,19 +28,32 @@ async function main() {
       });
     }
 
-    // There is future work in identifying the kind of files we're working with and their delimiters. Right now for DTRS we're using TXT files with ';' as a delimiter.
-    // TODO - add a way to identify the kind of files we're working with and their delimiters.
-
-    // We want to reach file and push the contents to files_data.
+    // Use Promises to read files asynchronolously. Use the resolved values to create a structured dataset: conversation_data_all.
+    let promises = [];
     files_to_read.forEach(file => {
-      let reader = new FileReader();
-      reader.onload = function (e) {
-        let datum = d3.dsvFormat(';').parse(reader.result);
-        files_data.push(datum);
-      };
-      reader.readAsText(file);
+      console.log(`reading ${file.name}...`);
+      let file_promise = new Promise(resolve => {
+        let reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = () => resolve(reader.result);
+      });
+      promises.push(file_promise);
     });
-    console.log(files_data);
+    // Once all files have been read, create a structured dataset to work with.
+    Promise.all(promises).then(file_contents => {
+      console.log(file_contents);
+      file_contents.forEach(file_content => {
+        let conversation_data = d3.dsvFormat(';').parse(file_content);
+        conversation_data.forEach(line => {
+          conversation_data_all.push(line);
+        });
+      });
+      console.log(conversation_data_all);
+    });
+
+    //   console.log(files_to_read);
+    // let data = await read_conversations_to_data(files_to_read);
+    // console.log(data);
   });
 }
 main();
